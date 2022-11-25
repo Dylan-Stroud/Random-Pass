7f969208f92b2c39a5ed3685d04bf4d33aa16c7c
Dylan-Stroud <dylan@strouds.net>
2022-11-25 10:19:38 +1000

    added text area for input

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,16 @@
+const header = document.getElementById("header");
+const inputArea = document.getElementById("inputArea");
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+
+  header.textContent = "How many characters?"
+  inputArea.style.display  = "flex";
+  generateBtn.addEventListener("characters", );
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
