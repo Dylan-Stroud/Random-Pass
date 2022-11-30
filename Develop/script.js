4af0f356973b5ca6f0679af18d2f53dcf41886c6
Dylan <dylan@strouds.net>
2022-11-30 19:17:26 +1000

    added logic for password generation

--- Develop/script.js
+++ Develop/script.js
@@ -7,30 +7,63 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+
+  // Input Prompts
   var numChar = prompt("How many characters in the password?");
-  var spChar = prompt("include special characters?");
   var numbers = prompt("include numbers?");
-  if (numbers === "yes"){
-    numbers = true;
-  }else{
-    numbers = false;
-  }
-  if (spChar === "yes"){
+  var spChar = prompt("include special characters?");
+
+  //
+   if (spChar === "yes"){
     spChar = true;
   }else{
     spChar = false;
   }
 
+  if (numbers === "yes"){
+    numbers = true;
+  }else{
+    numbers = false;
+  }
+
   
-  var password = generatePassword();
+  var password = generatePassword(numChar,spChar,numbers);
+  
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
 
-function generatePassword(){
+function generatePassword(l, spChar, num){
+  var charset = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var nmCharset = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z","1","2","3","4","5","6","7","8","9","0"];
+  var spCharset = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z","!","@","#","$","%","^","&","*","(",")","-","_","=","+","[","{","]","}","/","|","~","]",",","<",".",">","?"];
+  var spnmCharset = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z","!","@","#","$","%","^","&","*","(",")","-","_","=","+","[","{","]","}","/","|","~","]",",","<",".",">","?","1","2","3","4","5","6","7","8","9","0"];
+  t = [];
+  if(num == true && spChar == true){
+    t = spnmCharset;
+  }else if (num == true && spChar == false){
+    console.log("numonly");
+    t = nmCharset;
 
+  }else if (num == false && spChar == true){
+    t = spCharset
+    console.log("spOnly")
+  }else{
+    t = charset
+  }
+  password = [];
+
+  for(var i = 0; i<l; i++){
+    rand = Math.random()* t.length;
+    randChar = t[Math.round(rand)];
+    password.push(randChar);
+    
+  }
+  
+
+  return password.join("");
 
 }
 
